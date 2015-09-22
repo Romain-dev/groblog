93ef869da840b490f93f518859a904206d2a623d
Romain <galli.romain24@gmail.com>
2015-09-22 15:37:32 +0200

    CRUD 'R'

--- src/Ema/GroBlogBundle/Controller/BlogController.php
+++ src/Ema/GroBlogBundle/Controller/BlogController.php
@@ -3,17 +3,46 @@
 namespace Ema\GroBlogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Ema\GroBlogBundle\Entity\Post;
 
 class BlogController extends Controller
 {
     public function indexAction()
     {
+
+        $posts = $this->getLastTenPosts();
+        return $this->render('EmaGroBlogBundle:Blog:index.html.twig',array('posts' => $posts));
+    }
+
+    public function createAction()
+    {
+        $this->createPost("tonton","tata","neuveu");
+
         return $this->render('EmaGroBlogBundle:Blog:index.html.twig');
     }
 
     public function detailAction($iddupost)
     {
         return $this->render('EmaGroBlogBundle:Blog:detail.html.twig',array('id' => $iddupost));
+    }
+
+    private function createPost($titre, $urlAlias, $content)
+    {
+        $post = new Post();
+        $post->setTitre($titre);
+        $post->setUrlAlias($urlAlias);
+        $post->setContent($content);
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($post);
+        $em->flush();
+    }
+
+    private function getLastTenPosts()
+    {
+        $repository = $this->getDoctrine()->getRepository('EmaGroBlogBundle:Post');
+        $posts = $repository->findBy(array(),array('id' => 'DESC'),10);
+        return $posts;
     }
 
 }
---
+++ src/Ema/GroBlogBundle/Entity/Post.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Ema\GroBlogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Post
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Post
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="urlAlias", type="string", length=255)
+     */
+    private $urlAlias;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="content", type="text")
+     */
+    private $content;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return Post
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set urlAlias
+     *
+     * @param string $urlAlias
+     *
+     * @return Post
+     */
+    public function setUrlAlias($urlAlias)
+    {
+        $this->urlAlias = $urlAlias;
+
+        return $this;
+    }
+
+    /**
+     * Get urlAlias
+     *
+     * @return string
+     */
+    public function getUrlAlias()
+    {
+        return $this->urlAlias;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return Post
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+}
+
